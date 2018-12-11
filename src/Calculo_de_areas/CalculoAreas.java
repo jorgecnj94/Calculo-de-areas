049a4f499b8344c27956f5827810451cc9ff848a
Jorge Bedmar <jorgecnj_1994@hotmail.com>
2018-12-11 14:35:01 +0100

    Menu + triangulo

---
+++ src/Calculo_de_areas/CalculoAreas.java
@@ -0,0 +1,63 @@
+/**
+ * @author Jorge Bedmar
+ *
+ */
+package Calculo_de_areas;
+import java.util.Scanner;
+
+public class CalculoAreas {
+	//
+	Scanner sc = new Scanner (System.in);
+	
+	private int opcion = 1;
+
+	private double base   = 0.0;
+	private double altura = 0.0;
+	private double base2  = 0.0;
+	
+	private double area   = 0.0;
+	
+	
+	public void menu(){
+		
+		System.out.println (" \t\t Cálculo de áreas\n________________________________\n ");
+		System.out.println ("1. Calcular el área de un triángulo\n2. Calcular el área de un trapecio");
+		System.out.println ("3. Calcular el área de un rectángulo\n4. Salir\n\n Introduzca su opcion: ");
+		
+		String cadena = sc.next();
+		opcion = Integer.parseInt(cadena);
+		
+		while(opcion == 1 || opcion == 2 || opcion == 3) {
+			
+			if(opcion == 1) {
+				
+				System.out.println("\n Introduzca la base y la altura del triángulo para calcular su area");
+				if(sc.hasNextDouble()) {
+					base = sc.nextDouble();
+					if(sc.hasNextDouble()) {
+						altura = sc.nextDouble();
+					}
+				}
+				area = areaTriangulo(base, altura);
+				
+				if(area != 0.0) {
+					System.out.printf("\n El área del triángulo es %.2f", area);
+				}else {
+					System.out.println("\n No se puede calcular el área del triángulo debido a datos de entrada erróneos");
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public double areaTriangulo(double b, double a) {
+		
+		if(b > 0 && a > 0) {
+			return b*a/2.0;
+		}
+		return 0.0;
+	}
+
+}
---
+++ src/Calculo_de_areas/TestCalculoAreas.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Calculo_de_areas;
+
+/**
+ * @author matinal
+ *
+ */
+public class TestCalculoAreas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		CalculoAreas.menu();
+		
+		
+		//sc.close();
+	}
+
+}
